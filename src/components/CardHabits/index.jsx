35b22b27cb03952706304548729f9e84491f4eb3
Eduardo Paiva <eduardopgz@yahoo.com>
2021-12-16 10:21:33 -0300

    fix: token missing

--- src/components/CardHabits/index.jsx
+++ src/components/CardHabits/index.jsx
@@ -18,40 +18,44 @@ import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import api from "../../services/api";
 import { useState } from "react";
-import {useUser} from "../../providers/User";
-import CloseIcon from '@mui/icons-material/Close';
+import { useUser } from "../../providers/User";
+import CloseIcon from "@mui/icons-material/Close";
 import Close from "@mui/icons-material/Close";
 
-const CardHabits = ({habits, setEditiHabits, setOpemModal}) => {
-    
-    habits.sort((a,b) => b.how_much_achieved - a.how_much_achieved);
+const CardHabits = ({ habits, setEditiHabits, setOpemModal }) => {
+  const token = useState(JSON.parse(localStorage.getItem("token")) || "");
 
-    const {updateHabit, deleteHabit} = useUser();
+  habits.sort((a, b) => b.how_much_achieved - a.how_much_achieved);
+
+  const { updateHabit, deleteHabit } = useUser();
 
   const updateHowMuchAchieved = (item) => {
     let howMuch = 0;
 
-        item.frequency.toLocaleLowerCase() === "diária" ? 
-        howMuch = 3.34 : item.frequency.toLocaleLowerCase() === "mensal" ?
-        howMuch = 8.34 : item.frequency.toLocaleLowerCase() === "semanal" ? 
-        howMuch = 4.24 : item.frequency.toLocaleLowerCase() === "anual" ? 
-        howMuch = 20 : howMuch = 10;
-        
-        if (item.how_much_achieved+howMuch >= 100) {         
-            const data = {
-                how_much_achieved: 100,
-                achieved: true  
-            };
-            updateHabit(data, item.id);
-            } 
-            if (item.how_much_achieved+howMuch < 100) {
+    item.frequency.toLocaleLowerCase() === "diária"
+      ? (howMuch = 3.34)
+      : item.frequency.toLocaleLowerCase() === "mensal"
+      ? (howMuch = 8.34)
+      : item.frequency.toLocaleLowerCase() === "semanal"
+      ? (howMuch = 4.24)
+      : item.frequency.toLocaleLowerCase() === "anual"
+      ? (howMuch = 20)
+      : (howMuch = 10);
 
-                const number = Math.round(item.how_much_achieved + howMuch)
-                const data = {
-                    how_much_achieved: number
-                };
-                updateHabit(data, item.id);
-            };
+    if (item.how_much_achieved + howMuch >= 100) {
+      const data = {
+        how_much_achieved: 100,
+        achieved: true,
+      };
+      updateHabit(data, item.id);
+    }
+    if (item.how_much_achieved + howMuch < 100) {
+      const number = Math.round(item.how_much_achieved + howMuch);
+      const data = {
+        how_much_achieved: number,
+      };
+      updateHabit(data, item.id);
+    }
 
     if (item.how_much_achieved + howMuch >= 100) {
       console.log("100%");
@@ -86,59 +90,77 @@ const CardHabits = ({habits, setEditiHabits, setOpemModal}) => {
     }
   };
 
-    const opemEditHabits = (id) => {
-        setOpemModal(true);
-        setEditiHabits(id);
-    };
+  const opemEditHabits = (id) => {
+    setOpemModal(true);
+    setEditiHabits(id);
+  };
 
-    return(
-        <Conteiner>
-            {habits.map((item, index) => (
-                <CardConteiner key = {index}>
-                    <IconConteiner  category = {item.category} 
-                                    onClick = {() => opemEditHabits([item.id, item.title])}>
-                        {
-                        item.category.toLocaleLowerCase() === "saúde" ?
-                            <VolunteerActivismIcon/> : 
-                        item.category.toLocaleLowerCase() === "esporte" ?
-                            <SkateboardingIcon/> :
-                        item.category.toLocaleLowerCase() === "lazer" ?
-                            <IcecreamIcon/> :
-                        item.category.toLocaleLowerCase() === "trabalho" ?
-                            <HailIcon/> : <QuestionMarkIcon/>                            
-                            }
-                    </IconConteiner>
+  return (
+    <Conteiner>
+      {habits.map((item, index) => (
+        <CardConteiner key={index}>
+          <IconConteiner
+            category={item.category}
+            onClick={() => opemEditHabits([item.id, item.title])}
+          >
+            {item.category.toLocaleLowerCase() === "saúde" ? (
+              <VolunteerActivismIcon />
+            ) : item.category.toLocaleLowerCase() === "esporte" ? (
+              <SkateboardingIcon />
+            ) : item.category.toLocaleLowerCase() === "lazer" ? (
+              <IcecreamIcon />
+            ) : item.category.toLocaleLowerCase() === "trabalho" ? (
+              <HailIcon />
+            ) : (
+              <QuestionMarkIcon />
+            )}
+          </IconConteiner>
 
-                    <BodyConteiner>
-                        <ConteinerDiscription>
-                            <div>
-                                <Name>{item.title.slice(0,1).toUpperCase().concat(item.title.slice(1))}</Name>
-                                <Category   category = {item.category} >
-                                    {item.category.slice(0,1).toUpperCase().concat(item.category.slice(1))}
-                                </Category>
-                            </div>
-                            {item.how_much_achieved === 100 ?
-                                <CheckinConteiner   category = {item.category} 
-                                                    onClick = {() => deleteHabit(item.id)} >
-                                    <Close/>
-                                </CheckinConteiner>
-                                :
-                                <CheckinConteiner   category = {item.category} 
-                                                     onClick = {() => updateHowMuchAchieved(item)} >
-                                    <CheckCircleIcon/>
-                                </CheckinConteiner>}
-                        </ConteinerDiscription>
-                        <Bar>
-                            <ProgressBar    progress = {item.how_much_achieved} 
-                                            category = {item.category}>
-                                <p>{item.how_much_achieved.toFixed()}%</p>
-                            </ProgressBar>
-                        </Bar>
-                    </BodyConteiner>
-                </CardConteiner>
-            ))}
-        </Conteiner>
-    )
+          <BodyConteiner>
+            <ConteinerDiscription>
+              <div>
+                <Name>
+                  {item.title
+                    .slice(0, 1)
+                    .toUpperCase()
+                    .concat(item.title.slice(1))}
+                </Name>
+                <Category category={item.category}>
+                  {item.category
+                    .slice(0, 1)
+                    .toUpperCase()
+                    .concat(item.category.slice(1))}
+                </Category>
+              </div>
+              {item.how_much_achieved === 100 ? (
+                <CheckinConteiner
+                  category={item.category}
+                  onClick={() => deleteHabit(item.id)}
+                >
+                  <Close />
+                </CheckinConteiner>
+              ) : (
+                <CheckinConteiner
+                  category={item.category}
+                  onClick={() => updateHowMuchAchieved(item)}
+                >
+                  <CheckCircleIcon />
+                </CheckinConteiner>
+              )}
+            </ConteinerDiscription>
+            <Bar>
+              <ProgressBar
+                progress={item.how_much_achieved}
+                category={item.category}
+              >
+                <p>{item.how_much_achieved.toFixed()}%</p>
+              </ProgressBar>
+            </Bar>
+          </BodyConteiner>
+        </CardConteiner>
+      ))}
+    </Conteiner>
+  );
 };
 
 export default CardHabits;
